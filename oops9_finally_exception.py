f64d1948431b60bf73e75561cb3645c0c3c4b48e
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-09-30 11:37:36 +0530

    try except - finally

---
+++ oops9_finally_exception.py
@@ -0,0 +1,20 @@
+# try:
+#     a = int(input("input number"))
+#     b = int(input("input number"))
+#     c=a/b
+#     print("Result is ",c)
+# except Exception as e:
+#     print(e)
+
+try:
+    a = int(input("input number"))
+    b = int(input("input number"))
+    c=a/b
+    print("Result is ",c)
+    list1=[2,3]
+    i=int(input("enter index"))
+    print(list1[i])
+except Exception as e:
+    print(e)
+finally:
+    print("finally")
